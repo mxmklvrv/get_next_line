e12b7db5b3d8888956c57ad2657c32f9b0390ff6
Maxim Kleverov <mklevero@student.hive.fi>
2025-05-19 18:58:10 +0300

    first steps

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,27 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 16:48:33 by mklevero          #+#    #+#             */
-/*   Updated: 2025/05/16 16:48:35 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/05/19 18:57:42 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char		*result;
+	char		*buffer;
+	static char	*leftowers;
+
+	result = NULL;
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	if (BUFFER_SIZE <= 0 || fd < 0)
+	{
+		free(buffer);
+		buffer = NULL;
+		return (NULL);
+	}
+	result = // pass  to the function which fill the result.
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,20 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 16:49:54 by mklevero          #+#    #+#             */
-/*   Updated: 2025/05/16 16:49:56 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/05/19 18:57:45 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 69
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
