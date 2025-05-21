6211451f6e2dc8a25be7e3bff2ab5d9a5e7b0412
Maxim Kleverov <maximkleverov@icloud.com>
2025-05-21 14:10:53 +0300

    it works, need to check for leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,57 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 16:48:33 by mklevero          #+#    #+#             */
-/*   Updated: 2025/05/20 15:49:42 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/05/21 14:09:27 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char *ft_get_line(int fd, char *leftovers, char *buffer)
+{
+	ssize_t byte_was_read;
+	char *tmp;
+
+	byte_was_read = 1; 
+	while(byte_was_read > 0)
+	{
+		byte_was_read = read(fd, buffer, BUFFER_SIZE);
+		if(byte_was_read == 0)
+			break; 
+		else if (byte_was_read < 0)
+			return (NULL);
+		buffer[byte_was_read] = '\0';
+		if (!leftovers)
+			leftovers = ft_strdup("");
+		tmp = leftovers;
+		leftovers = ft_strjoin(tmp, buffer);
+		free(tmp);
+		tmp = NULL;
+		if (ft_strchr(buffer, '\n'))
+			break;
+	}
+	return (leftovers);
+}
+
+char *ft_save_leftovers(char *full_line)
+{
+	char *remainings;
+	char *ptr_nl;
+
+	ptr_nl = ft_strchr(full_line, '\n');
+	if (ptr_nl)
+	{
+		remainings = ft_strdup(ptr_nl + 1);
+		if(*remainings == '\0')
+		{
+			free(remainings);
+			remainings = NULL;
+		}
+		*(ptr_nl + 1) = '\0';
+		return (remainings);
+	}
+	return (NULL);
+}
 
 char	*get_next_line(int fd)
 {
@@ -22,17 +68,31 @@ char	*get_next_line(int fd)
 	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
 		return (NULL);
-	if (BUFFER_SIZE <= 0 || fd < 0)
+	if (BUFFER_SIZE <= 0 || fd < 0) // might check if fd is closed as well. 
 	{
 		free(buffer);
 		buffer = NULL;
 		return (NULL);
 	}
-	result = get_line(fd, remainings, buffer); 
+	result = ft_get_line(fd, leftowers, buffer);
+	free(buffer);
+	buffer = NULL;
+	if(!result)
+		return(NULL);
+	leftowers = ft_save_leftovers(result);
+	return (result);
 }
 
-char *get_line(int fd, char *remainings, char *buffer)
+
+
+int main (void)
 {
+    char *line;
+    int fd;
 
-
+    fd = open("text.txt", O_RDONLY);
+    line = get_next_line(fd);
+    printf("%s\n", line);
+	line = get_next_line(fd);
+	printf("%s\n", line);
 }
